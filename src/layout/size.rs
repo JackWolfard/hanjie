de7b95443f71565adea70169ecbdaabe6b5367b8
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2025-05-06 02:01:42 -0700

    2025-05-06: in-progress. not sure what the layout is doing.

--- src/layout/size.rs
+++ src/layout/size.rs
@@ -2,8 +2,10 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+use std::cell::RefCell;
+
 use bevy::{
-    ecs::entity::EntityHashSet,
+    ecs::entity::{EntityHashMap, EntityHashSet},
     math::AspectRatio,
     prelude::*,
     window::{self, PrimaryWindow},
@@ -53,11 +55,39 @@ impl Resizable {
     }
 }
 
+struct ResizeTarget<'a> {
+    entity: Entity,
+    bounding_box: &'a BoundingBox,
+}
+
+trait ResizeEvent {}
+
 impl Resizable {
+    fn try_resize(&self, target: ResizeTarget, event: &impl ResizeEvent) -> Option<EntityResized> {
+        // self.constrain_to_aspect_ratio(
+        // self.width.resize(reference, reference.width()),
+        // self.height.resize(reference, reference.height()),
+        // )
+        None
+    }
+
     fn apply_if(&self, reference: Reference, reference_size: Vec2, target_size: Vec2) -> Vec2 {
         let width = self.width.apply_if(reference, reference_size.x);
         let height = self.height.apply_if(reference, reference_size.y);
         self.finalize(target_size, width, height)
+    }
+
+    fn constrain_to_aspect_ratio(&self, width: f32, height: f32) -> Vec2 {
+        self.aspect_ratio
+            .and_then(|aspect_ratio| {
+                let new_aspect_ratio: f32 = AspectRatio::new(width, height).into();
+                match new_aspect_ratio.total_cmp(&aspect_ratio) {
+                    std::cmp::Ordering::Less => Some(Vec2::new(width, width / aspect_ratio)),
+                    std::cmp::Ordering::Equal => None,
+                    std::cmp::Ordering::Greater => Some(Vec2::new(height * aspect_ratio, height)),
+                }
+            })
+            .unwrap_or(Vec2::new(width, height))
     }
 
     fn apply_aspect_ratio(&self, size: Vec2) -> Vec2 {
@@ -122,6 +152,14 @@ pub struct ResizableField {
 }
 
 impl ResizableField {
+    fn try_resize(&self) -> Option<f32> {
+        None
+    }
+
+    fn resize(&self, reference: f32) -> f32 {
+        self.constraint.apply(reference)
+    }
+
     fn apply_if(&self, reference: Reference, size: f32) -> Option<f32> {
         if self.reference == reference {
             Some(self.constraint.apply(size))
@@ -171,6 +209,8 @@ impl ResizableField {
             _ => None,
         }
     }
+
+    fn _intersect(&self, x: Entity, y: Entity) {}
 }
 
 pub enum Constraint {
@@ -202,6 +242,31 @@ pub enum EntityReference {
     Parent,
 }
 
+#[derive(Debug, Clone, Copy)]
+enum KnownReference {
+    Intersect(Vec2, Vec2),
+    Parent(Vec2),
+    Window(Vec2),
+}
+
+impl KnownReference {
+    fn size(&self) -> Vec2 {
+        match self {
+            KnownReference::Intersect(a, b) => (*a - *b).abs(),
+            KnownReference::Parent(a) => *a,
+            KnownReference::Window(a) => *a,
+        }
+    }
+
+    fn width(&self) -> f32 {
+        self.size().x
+    }
+
+    fn height(&self) -> f32 {
+        self.size().y
+    }
+}
+
 impl EntityReference {
     fn entity(&self, target: Entity, parent_q: &Query<&Parent>) -> Option<Entity> {
         let mut ancestors = parent_q.iter_ancestors(target);
@@ -210,12 +275,25 @@ impl EntityReference {
             EntityReference::Parent => ancestors.next(),
         }
     }
+
+    fn size(
+        &self,
+        target: Entity,
+        parent_q: &Query<&Parent>,
+        bounding_box_q: &Query<&BoundingBox>,
+    ) -> Option<Vec2> {
+        self.entity(target, parent_q)
+            .and_then(|e| bounding_box_q.get(e).ok())
+            .map(|b| b.size())
+    }
 }
 
 #[derive(Event, Debug)]
 pub struct WindowResized {
     pub size: Vec2,
 }
+
+impl ResizeEvent for WindowResized {}
 
 #[derive(Event, Debug)]
 pub struct EntityResized {
@@ -224,18 +302,27 @@ pub struct EntityResized {
     pub dependencies: EntityHashSet,
 }
 
+impl ResizeEvent for EntityResized {}
+
+#[derive(Event, Debug)]
+pub struct EventualEntityResized {
+    pub entity: Entity,
+}
+
 fn engine_window_resize(
-    mut ev_engine: EventReader<window::WindowResized>,
+    mut engine_ev: EventReader<window::WindowResized>,
     window_q: Query<Entity, With<PrimaryWindow>>,
-    mut ev_window: EventWriter<WindowResized>,
+    mut window_ev: EventWriter<WindowResized>,
 ) {
-    for event in ev_engine.read() {
-        for window in window_q.iter() {
-            if window == event.window {
-                ev_window.send(WindowResized {
-                    size: Vec2::new(event.width, event.height),
-                });
-            }
+    if let Ok(primary_window) = window_q.get_single() {
+        if let Some(event) = engine_ev
+            .read()
+            .filter(|event| event.window == primary_window)
+            .last()
+        {
+            window_ev.send(WindowResized {
+                size: Vec2::new(event.width, event.height),
+            });
         }
     }
 }
@@ -245,25 +332,28 @@ fn window_resize(
     resize_q: Query<(Entity, &BoundingBox, &Resizable)>,
     mut entity_ev: EventWriter<EntityResized>,
 ) {
-    for event in window_ev.read() {
-        for (entity, bounding_box, resizable) in &resize_q {
-            let old_size = bounding_box.size();
-            let size = resizable.apply_if(Reference::Window, event.size, old_size);
-            if matches!(resizable.width.reference, Reference::Window)
-                || matches!(resizable.height.reference, Reference::Window)
-            {
-                debug!(
-                    "Window resize for {entity:?}: {old_size:?} != {size:?}, reference = {:?}",
-                    event.size
-                );
-                entity_ev.send(EntityResized {
-                    entity,
-                    size,
-                    dependencies: Default::default(),
-                });
-            }
-        }
+    if let Some(window) = window_ev.read().last() {
+        entity_ev.send_batch(
+            resize_q
+                .iter()
+                .filter_map(|(entity, bounding_box, resizable)| {
+                    resizable.try_resize(
+                        ResizeTarget {
+                            entity,
+                            bounding_box,
+                        },
+                        window,
+                    )
+                }),
+        );
     }
+}
+
+fn _entity_resize(mut entity_ev: EventReader<EntityResized>) {
+    let unresolved_entities: EntityHashMap<Vec2> = entity_ev
+        .read()
+        .map(|event| (event.entity, event.size))
+        .collect();
 }
 
 fn parent_resize(
@@ -325,6 +415,51 @@ fn intersect_resize(
     }
 
     param_set.p1().send_batch(new_events);
+}
+
+// for an entity
+// 1. if all deps are not in resized_entities, then use calculated size
+// 2. if at least one dep is in resized_entities, then recalculate new size
+//     a. that dep / those deps, must all have finalized sizes
+// 3. multiple resize events for an entity is valid, but this might
+//    indicate an entity's deps were also resized in this batch. Probably a
+//    recalculate. Can result in error if invariant of entity always having
+//    same deps is violated. For instance, event 1 being c = intersect(a, b)
+//    & event 2 being c = intersect(a, d) is invalid since deps are
+//    different between two events
+fn eventual_entity_resize(mut events: EventReader<EntityResized>) {
+    let mut unresolved_entities = EntityHashMap::<(Vec2, EntityHashSet)>::default();
+    for event in events.read() {
+        unresolved_entities.insert(event.entity, (event.size, event.dependencies.clone()));
+    }
+
+    let resized_entities = EntityHashSet::from_iter(unresolved_entities.keys().cloned());
+
+    unresolved_entities
+        .iter_mut()
+        .for_each(|(_, (_, dependencies))| dependencies.retain(|e| resized_entities.contains(e)));
+
+    let mut finalized_entities = EntityHashMap::<Vec2>::default();
+
+    unresolved_entities
+        .extract_if(|_, (_, dependencies)| dependencies.is_empty())
+        .for_each(|(entity, (size, _))| {
+            finalized_entities.insert(entity, size);
+        });
+
+    let finalized_cell = RefCell::new(finalized_entities);
+    while !unresolved_entities.is_empty() {
+        unresolved_entities
+            .extract_if(|_, (_, dependencies)| {
+                dependencies
+                    .iter()
+                    .all(|d| finalized_cell.try_borrow().is_ok_and(|h| h.contains_key(d)))
+            })
+            .for_each(|(entity, _)| {
+                // recalculate entity size based on dependencies finalized values
+                finalized_cell.borrow_mut().insert(entity, Vec2::splat(0.0));
+            });
+    }
 }
 
 fn entity_resize(
